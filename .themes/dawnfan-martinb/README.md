d166de90639621322eb8d0049e23f4764018b170
D-W- <phoenix.seek@gmail.com>
2015-05-04 17:47:07 +0800

    new theme

---
+++ .themes/dawnfan-martinb/README.md
@@ -0,0 +1,32 @@
+# Made some edits to the classic-martinb.
+# dawnfan-martinb
+
+A simple Octopress theme. I made some edits to the original classic theme to slim it down a little and add different navigation links.
+
+![classic-martinb theme screenshot](/classic-martinb.png)
+
+## Installation
+
+```shell
+$ cd octopress
+$ git submodule add https://github.com/dawnfan/dawnfan-martinb.git .themes/dawnfan-martinb
+$ rake install['dawnfan-martinb']
+$ rake generate
+```
+
+If using zsh, there might by globbing issues running `$ rake install['dawnfan-martinb']`, running `$ rake "install[dawnfan-martinb]"` might be required.
+
+## Caveats
+To get social icons for LinkedIn and Facebook, add the variables ``facebook_user`` and ``linkedin_user`` with corresponding values to your ``_config.yml`` file, as per PR [fapper/classic-martinb#2](https://github.com/fapper/classic-martinb/pull/2).
+
+## License
+(The MIT License)
+
+Copyright © 2013 Martin Bjeldbak Madsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ‘Software’), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ .themes/dawnfan-martinb/source/javascripts/dawnfan.js
@@ -0,0 +1,81 @@
+window.onload = init();
+function init(){
+  var scrollTop = 0;
+  window.onscroll = function(){
+    var itNav = $('.header-right');
+    var itMain = document.getElementById('content');
+    //var itList = document.getElementById('list');
+    var headerHeight = $('header').outerHeight()-$('.header-right').outerHeight();//包括padding和border
+    scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollTop < headerHeight){
+      //itNav.style.position = 'fixed';
+      //itNav.className = '';
+      itNav.removeClass('fixed-nav');
+      itMain.className = '';
+      //itList.className = '';
+    }
+    else{//当header滑到可视区域外，将导航栏固定在页面上
+      //itNav.className = 'fixed-nav';
+      itNav.addClass('fixed-nav');
+      //document.getElementById("main").style.marginTop="64px";
+      itMain.className = 'fixed-nav-main';
+      //itList.className = 'fixed-nav-list';
+    };
+  };
+}
+var onList = 0;
+function turnList(){//释放list
+  var itList = document.getElementById('list');
+  var itBtn = document.getElementById('btn-list');
+  var w=window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+  var h=window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+  var maxH = h;
+  var list_top = 0;
+  $('#list').css({'height': h-100+'px',
+                  'width': h/2});
+  // if(itList != null){//设置list最大高度
+  //   if(itList.className == ''){
+  //     list_top = $('header').outerHeight();
+  //   }else{
+  //     list_top = $('.fixed-nav').outerHeight();
+  //   }
+  //   maxH = h-list_top;
+  //   itList.style.maxHeight = maxH+'px';
+  //   // $('#list.fixed-nav-list').css('top',$('.fixed-nav').outerHeight());
+  //   // $('#list').css('top',$('header').outerHeight());
+  //   itList.style.top = list_top+'px';
+  // }     
+  if(onList == 0){//将list设为可见，并转转转（tosel）
+    itBtn.className = 'tosel';
+    itList.className = 'on';
+    // itList.style.opacity = 1;
+    // itList.style.visibility = 'visible';
+    onList = 1;
+  }else{
+    itBtn.className = '';
+    itList.className = 'off';
+    // itList.style.opacity = 0;
+    // itList.style.visibility = 'hidden';
+    onList = 0;
+  };
+  // $('#btn-list.tosel').css({
+  //     'transform': 'translateY('+h/2+'px) rotate(450deg)',
+  //     '-ms-transform': 'translateY('+h/2+'px) rotate(450deg)',
+  //     '-moz-transform': 'translateY('+h/2+'px) rotate(450deg)', /* Firefox 4 */
+  //     '-webkit-transform': 'translateY('+h/2+'px) rotate(450deg)', /* Safari and Chrome */
+  //     '-o-transform': 'translateY('+h/2+'px) rotate(450deg)'/* Opera */
+  // });
+}
+
+// $(function () {//转转转变色
+//   $('#btn-list').mouseenter(function(){
+//     $('#btn-img').attr('src','/images/list_red.png');
+//   }).mouseleave(function(){
+//     $('#btn-img').attr('src','/images/list_white.png');
+//   });
+//   //$('#list').css('top',$('header').outerHeight());
+// });
